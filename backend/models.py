d3b87a2622e7ae98a2182210963660ead33ce408
Daniel <danielrmendonca04@gmail.com>
2026-05-06 12:23:41 -0300

    feat: adicionar endpoint v1 de métricas de produto

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, Float, Integer, String
 from database import Base
 
 
@@ -18,4 +18,62 @@ class Cliente(Base):
     pais_cliente = Column(String, nullable=True)
     origem_cliente = Column(String, nullable=True)
     idade = Column(Integer, nullable=True)
+
+# Tabelas
+class DimProduto(Base):
+    __tablename__ = "dim_produto"
+
+    id_produto = Column(String, primary_key=True, index=True)
+    nome_produto = Column(String, index=True)
+    categoria_produto = Column(String, index=True)
+    preco_produto = Column(Float, nullable=True)
+    fornecedor_produto = Column(String, nullable=True)
+    estoque_produto = Column(Integer, nullable=True)
+    produto_ativo = Column(Boolean, nullable=True)
+    faixa_preco = Column(String, nullable=True)
+
+class FatoVendas(Base):
+    __tablename__ = "fato_vendas"
+
+    id_pedido = Column(String, primary_key=True, index=True)
+    data_pedido = Column(String, nullable=True)
+    id_cliente = Column(String, index=True, nullable=True)
+    nome_cliente = Column(String, nullable=True)
+    id_produto = Column(String, index=True, nullable=True)
+    nome_produto = Column(String, nullable=True)
+    categoria_produto = Column(String, nullable=True)
+    quantidade_produto = Column(Integer, nullable=True)
+    valor_pedido = Column(Float, nullable=True)
+    metodo_pagamento = Column(String, nullable=True)
+    status_pedido = Column(String, nullable=True)
+
+
+class FatoAvaliacoes(Base):
+    __tablename__ = "fato_avaliacoes"
+
+    id_avaliacao = Column(String, primary_key=True, index=True)
+    id_cliente = Column(String, index=True, nullable=True)
+    nome_cliente = Column(String, nullable=True)
+    id_produto = Column(String, index=True, nullable=True)
+    nome_produto = Column(String, nullable=True)
+    id_pedido = Column(String, nullable=True)
+    nota_produto = Column(Float, nullable=True)
+    nota_nps = Column(Float, nullable=True)
+    recomenda_produto = Column(Boolean, nullable=True)
+    data_avaliacao = Column(String, nullable=True)
+
+
+class FatoSuporte(Base):
+    __tablename__ = "fato_suporte"
+
+    ticket_id = Column(String, primary_key=True, index=True)
+    id_cliente = Column(String, index=True, nullable=True)
+    nome_cliente = Column(String, nullable=True)
+    id_pedido = Column(String, index=True, nullable=True)
+    data_pedido = Column(String, nullable=True)
+    tipo_problema = Column(String, nullable=True)
+    data_abertura = Column(String, nullable=True)
+    data_resolucao = Column(String, nullable=True)
+    tempo_resolucao_horas = Column(Float, nullable=True)
+    agente_suporte = Column(String, nullable=True)
 
